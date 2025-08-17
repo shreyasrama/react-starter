9af78606e0d3181c28631d0bfe82b6c4eaf4b8c3
shreyasrama <shreyas.rama1@gmail.com>
2025-08-17 17:35:06 +1200

    add oxlint config file, update prettier config and add tsc check

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,5 +1,13 @@
-export default {
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
   $schema: "https://json.schemastore.org/prettierrc",
 
   plugins: ["@prettier/plugin-oxc", "prettier-plugin-tailwindcss"],
+
+  printWidth: 80,
 };
+
+export default config;
